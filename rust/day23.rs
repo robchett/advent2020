20ca6bd96eaa0b7e1186c6c7b8d8058f583d1c62
Rob Chett <robchett@gmail.com>
2020-12-23 15:00:05 +0000

    Document da 23, simplify the code too.

--- rust/day23.rs
+++ rust/day23.rs
@@ -13,10 +13,12 @@ fn test_run_23() {
 }
 
 pub fn run(input: String) -> Result<(i64, i64), &'static str> {
-
+    // Form a string from the results of the basic cup game to get the part 1 answer.
     let mut part1 = "0".to_owned();
+    // Play the cup game for the basic 9 digits and 100 rotations
     let cups1 = calculate_cups(input.to_owned(), 9, 100);
     let mut prev = 1;
+    // Starting (but skipping) 1, and rotating until we get back to 1, form a string of digets
     loop {
         let next = cups1[prev];        
         if next == 1 {
@@ -27,50 +29,68 @@ pub fn run(input: String) -> Result<(i64, i64), &'static str> {
         
     }
 
+    // Play the cup game with 1mil cups and 10mil rotations
     let cups2 = calculate_cups(input.to_owned(), 1_000_000, 10_000_000);
 
 
     return Ok((part1.parse::<i64>().unwrap(), (cups2[1] * cups2[cups2[1]]) as i64));
 }
 
+// Play the cup game with {size} cups, returns the final cup fomation after {count} rotations
+// Populate the first digits with the input.
 fn calculate_cups(input: String, size: usize, count: usize) -> Vec<usize> {
+    // Form a list of cups, the index is the digit, and the value is the number that follows it in the list
+    // Start with {size} blank cups, 0 index is unused, but leave it blank to avioid dealing with offsets.
     let mut cups = vec![0; size + 1];
+    // Parse the input as the starting cup positions
     let digits = input.chars().collect::<Vec<char>>().iter().map(|x| x.to_digit(10).unwrap() as usize).collect::<Vec<usize>>();
+    // The starting cursor is the first cup in the input
     let mut cursor = digits.first().unwrap().to_owned();
+
+    // Using our starting list, update our list with the next digit
     let mut prev = digits.first().unwrap().to_owned();
     for d in 1..digits.len() {
         cups[prev] = digits[d];
         prev = digits[d];
     }
+    // If we have more cups than inputs fill out the remainder as n -> n+1
     for d in (digits.len()+1)..size+1 {
         cups[prev] = d;
         prev = d;
     }
+    // Loop back out last cup by referencing the first.
     cups[prev] = cursor;
 
-    for i in 0..count {   
-        let mut dest = cursor;
-        let pop = cups[cursor];
-        let popped = vec![cups[cursor], cups[cups[cursor]], cups[cups[cups[cursor]]]];
+    // Loop for the total count
+    for _ in 0..count {   
+        // Extract the next 3 cups so we can make sure our destination isn't one of them
+        let pop1 = cups[cursor];
+        let pop2 = cups[pop1];
+        let pop3 = cups[pop2];
+        let next = cups[pop3];
 
+        // Work out where we're moving our popped list to
+        // If the destination is in our popped list, then try the next lowest value
+        // Loop back to the highest value if we reach the first
+        let mut dest = cursor;
         loop {
             dest -= 1;
             if dest == 0 {
                 dest = size;
             }
-            if !popped.contains(&dest) {
+            if dest != pop1 && dest != pop2 && dest != pop3 {
                 break;
             }
         }
-        let cursor5 = cups[cups[cups[cups[cursor]]]];
-        let cursor4 = cups[cups[cups[cursor]]];
-        let cursor2 = cups[cursor];
-        let dest1 = cups[dest];
+        // Join our current point to the one after 3 popped llist
+        cups[cursor] = next;
+        // Join the end of the popped list to one after the destination
+        cups[pop3] = cups[dest];
+        // Join the destination onto the popped list
+        cups[dest] = pop1;
 
-        cups[cursor]  = cursor5;
-        cups[cursor4] = dest1;
-        cups[dest]    = cursor2;
-        cursor = cursor5
+        // Move the cursor on to the next value
+        cursor = next
     }
     return cups;
 }
